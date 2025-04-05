20e595c7435ecb2335aaf8c1cfbeaa918a207b77
Kendaka <barengkendrick4@gmail.com>
2025-04-05 19:26:52 +0400

    please work man

--- server-side/config/database.js
+++ server-side/config/database.js
@@ -1,9 +1,10 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-console.log('DB_HOST:', process.env.DB_HOST); // Add this line for debugging
+console.log('DB_HOST:', process.env.DB_HOST); 
 
 const sequelize = new Sequelize(process.env.DB_HOST, {
+  dialect: 'postgres', 
   dialectOptions: {
     ssl: {
       require: true,
