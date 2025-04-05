df3a8c7ce5a906220d4fd1b6d57c14e5acb586aa
Kendaka <barengkendrick4@gmail.com>
2025-04-05 19:43:54 +0400

    trying the break config

--- server-side/config/database.js
+++ server-side/config/database.js
@@ -3,14 +3,22 @@ require('dotenv').config();
 
 console.log('DB_HOST:', process.env.DB_HOST); 
 
-const sequelize = new Sequelize(process.env.DB_HOST, {
-  dialect: 'postgres', 
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    }
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   }
-});
+);
+
 
 module.exports = sequelize;
