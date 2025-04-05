24520ce2df590db41f857ac09206210ccb1f8c33
Kendaka <barengkendrick4@gmail.com>
2025-04-05 20:21:37 +0400

    update the api.js

--- client-side/src/services/api.js
+++ client-side/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api'; 
+const API_URL = 'https://pennyrise.onrender.com'; 
 
 // User Authentication
 export const registerUser = async (userData) => {
