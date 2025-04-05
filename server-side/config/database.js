b840ae0a881e9422033629805dfe4f7c669f34e8
Kendaka <barengkendrick4@gmail.com>
2025-04-05 19:07:48 +0400

    test

--- server-side/config/database.js
+++ server-side/config/database.js
@@ -1,13 +1,15 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+console.log('DB_HOST:', process.env.DB_HOST); // Add this line for debugging
+
 const sequelize = new Sequelize(process.env.DB_HOST, {
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, 
+      rejectUnauthorized: false,
     }
   }
-})
+});
 
 module.exports = sequelize;
