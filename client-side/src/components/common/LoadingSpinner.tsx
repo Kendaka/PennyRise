07f0eb4f409f61c24437f6806240477e5f729841
Kendaka <barengkendrick4@gmail.com>
2025-04-27 20:01:33 +0400

    loading spiiner updated with react.fc

--- client-side/src/components/common/LoadingSpinner.tsx
+++ client-side/src/components/common/LoadingSpinner.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const LoadingSpinner = () => {
+const LoadingSpinner: React.FC = () => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="loader"></div>
