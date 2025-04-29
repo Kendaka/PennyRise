621244fd4c44d301bae46e55fa5f253dfd6e805c
Kendaka <barengkendrick4@gmail.com>
2025-04-29 15:51:26 +0400

    Home

---
+++ client-side/src/pages/Home/Home.tsx
@@ -0,0 +1,13 @@
+// Home.tsx
+import React from "react";
+import Hero from "../../components/sections/Hero/Hero";
+
+const Home: React.FC = () => {
+  return (
+    <div className="bg-background min-h-screen">
+      <Hero />
+    </div>
+  );
+};
+
+export default Home;
