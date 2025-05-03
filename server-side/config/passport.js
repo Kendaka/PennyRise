0aa84598de5d6f9d37de01150f5576445ae38aa0
Kendaka <barengkendrick4@gmail.com>
2025-05-03 10:55:51 +0400

    added configure

--- server-side/config/passport.js
+++ server-side/config/passport.js
@@ -1,3 +1,4 @@
+// This file configures the Google OAuth strategy for Passport.js
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const User = require('../models/User');
