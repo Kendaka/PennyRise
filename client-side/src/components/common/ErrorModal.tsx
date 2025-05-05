fbafc4c46567e502e40285afea363ef45d826711
Kendaka <barengkendrick4@gmail.com>
2025-05-05 14:01:23 +0400

    error modal component

--- client-side/src/components/common/ErrorModal.tsx
+++ client-side/src/components/common/ErrorModal.tsx
@@ -7,6 +7,7 @@ interface ErrorModalProps {
   onClose: () => void;
 }
 
+// ErrorModal component definition
 const ErrorModal: React.FC<ErrorModalProps> = ({ message, onClose }) => {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
