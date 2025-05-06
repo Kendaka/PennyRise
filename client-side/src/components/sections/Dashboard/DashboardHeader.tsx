eb0f6c38c10f8c4ccb21eb48877e0b023b852b67
Kendaka <barengkendrick4@gmail.com>
2025-05-06 13:26:00 +0400

    dashboardheader comp

--- client-side/src/components/sections/Dashboard/DashboardHeader.tsx
+++ client-side/src/components/sections/Dashboard/DashboardHeader.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import defaultProfilePic from '../../../assets/images/defaultProfilePic.png';
 
+// Define the props for the DashboardHeader component
 interface DashboardHeaderProps {
   userName: string;
 }
