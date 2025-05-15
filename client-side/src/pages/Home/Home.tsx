54438ea7a27ab5493a54ef6ff9c7c564d0cf3c4b
Kendaka <barengkendrick4@gmail.com>
2025-05-15 21:17:15 +0400

    main page

--- client-side/src/pages/Home/Home.tsx
+++ client-side/src/pages/Home/Home.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Hero from "../../components/sections/Hero/Hero";
 
+// This is the main page of the application
 const Home: React.FC = () => {
   return (
     <div className="bg-background min-h-screen">
