b5ee75a7fcc68c22ba322852f0537fddc370ebdf
Kendaka <barengkendrick4@gmail.com>
2025-05-24 22:18:39 +0400

    use the user service to register or update the user based on google profile

--- server-side/config/passport.js
+++ server-side/config/passport.js
@@ -23,10 +23,11 @@ passport.deserializeUser(async (id, done) => {
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: 'http://localhost:5000/api/users/auth/google/callback',
+  callbackURL: 'http://localhost:5000/api/users/auth/google/callback', //
   scope: ['profile', 'email']
 }, async (accessToken, refreshToken, profile, done) => {
   console.log('Google profile:', profile); 
+  // Use the userService to register or update the user based on the Google profile
   try {
     const user = await userService.registerOrUpdateGoogleUser(profile);
     done(null, user);
