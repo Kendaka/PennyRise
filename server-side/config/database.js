cf670ec16cce6425fed83b050489766eaddb97b4
Kendaka <barengkendrick4@gmail.com>
2025-05-24 22:17:56 +0400

    authenticate the connection

--- server-side/config/database.js
+++ server-side/config/database.js
@@ -31,7 +31,7 @@ const sequelize = new Sequelize(
 // Test the connection
 async function testConnection() {
   try {
-    await sequelize.authenticate();
+    await sequelize.authenticate(); // Authenticate the connection
     console.log('Connection to the database has been established successfully.');
   } catch (error) {
     console.error('Unable to connect to the database:', error);
