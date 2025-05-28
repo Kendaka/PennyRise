fe3e474f95c84c29fbe4059315f3b6c79d56cf3e
Kendaka <barengkendrick4@gmail.com>
2025-05-28 21:33:49 +0400

    fixed the endpoint for requesting password

--- server-side/routes/authRoutes.js
+++ server-side/routes/authRoutes.js
@@ -3,7 +3,7 @@ const authController = require('../controllers/authController');
 
 const router = express.Router();
 
-router.post('/request-password-reset', authController.requestPasswordReset);
+router.post('/request-password-reset', authController.requestPasswordReset); // Endpoint to request a password reset
 router.post('/reset-password', authController.resetPassword);
 
 module.exports = router;
