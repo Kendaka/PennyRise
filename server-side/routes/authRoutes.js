491325e8d3ad91591d3006be66a809fc3bcf5ff8
Kendaka <barengkendrick4@gmail.com>
2025-05-28 21:34:09 +0400

    endpoint to reset functions

--- server-side/routes/authRoutes.js
+++ server-side/routes/authRoutes.js
@@ -4,6 +4,6 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.post('/request-password-reset', authController.requestPasswordReset); // Endpoint to request a password reset
-router.post('/reset-password', authController.resetPassword);
+router.post('/reset-password', authController.resetPassword); // Endpoint to reset the password
 
 module.exports = router;
