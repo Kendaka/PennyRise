1cb13cbc206c3974af883fffa38e980a686623b1
Kendaka <barengkendrick4@gmail.com>
2025-05-29 21:23:01 +0400

    fixed the endpoint that gets all savings goal

--- server-side/routes/savingsRoutes.js
+++ server-side/routes/savingsRoutes.js
@@ -5,7 +5,7 @@ const authenticateToken = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/create', authenticateToken, savingsController.createSavingsGoal); // Endpoint to create a new savings goal
-router.get('/', authenticateToken, savingsController.getSavingsGoals);
+router.get('/', authenticateToken, savingsController.getSavingsGoals); // Endpoint to get all savings goals
 router.put('/update', authenticateToken, savingsController.updateSavingsGoal);
 router.delete('/delete', authenticateToken, savingsController.deleteSavingsGoal);
 
