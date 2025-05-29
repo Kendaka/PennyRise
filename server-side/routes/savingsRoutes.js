434c59af04da9a6ed99e6c661594d30f3bd966b5
Kendaka <barengkendrick4@gmail.com>
2025-05-29 21:23:18 +0400

    endpoint to update an existing savings goal

--- server-side/routes/savingsRoutes.js
+++ server-side/routes/savingsRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/create', authenticateToken, savingsController.createSavingsGoal); // Endpoint to create a new savings goal
 router.get('/', authenticateToken, savingsController.getSavingsGoals); // Endpoint to get all savings goals
-router.put('/update', authenticateToken, savingsController.updateSavingsGoal);
+router.put('/update', authenticateToken, savingsController.updateSavingsGoal); // Endpoint to update an existing savings goal
 router.delete('/delete', authenticateToken, savingsController.deleteSavingsGoal);
 
 module.exports = router;
