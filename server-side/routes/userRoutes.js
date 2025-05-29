3e1bfb6a576bc2e38534411d14b41ced946413b0
Kendaka <barengkendrick4@gmail.com>
2025-05-29 21:47:42 +0400

    added a new route for updating profile info

--- server-side/routes/userRoutes.js
+++ server-side/routes/userRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/register', userController.register); // Endpoint to register a new user
 router.post('/login', userController.login); // Endpoint to log in an existing user
 router.put('/update-income-currency', authenticateToken, userController.updateIncomeAndCurrency); // Endpoint to update user's income and currency
-router.put('/update-profile', authenticateToken, userController.updateProfile);
+router.put('/update-profile', authenticateToken, userController.updateProfile); // Endpoint to update user's profile information
 router.put('/update-profile-picture', authenticateToken, userController.upload.single('profilePicture'), userController.updateProfilePicture);
 router.put('/update-onboarding-status', authenticateToken, userController.updateOnboardingStatus);
 router.put('/change-password', authenticateToken, userController.changePassword);
