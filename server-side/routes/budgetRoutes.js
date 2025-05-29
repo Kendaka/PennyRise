e9adbc7714e8d03c2ebd6131766c81575026a4fe
Kendaka <barengkendrick4@gmail.com>
2025-05-29 21:22:04 +0400

    endpoint to existing budget updated

--- server-side/routes/budgetRoutes.js
+++ server-side/routes/budgetRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/create', authenticateToken, budgetController.createBudget); // Endpoint to create a new budgets
 router.get('/', authenticateToken, budgetController.getBudgets); // Endpoint to get all budgets
-router.put('/update', authenticateToken, budgetController.updateBudget);
+router.put('/update', authenticateToken, budgetController.updateBudget); // Endpoint to update an existing budget
 router.delete('/delete', authenticateToken, budgetController.deleteBudget);
 
 module.exports = router;
