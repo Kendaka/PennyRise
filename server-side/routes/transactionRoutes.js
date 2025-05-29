9af1816c6561f700268d0df124f4e8e1df4869f0
Kendaka <barengkendrick4@gmail.com>
2025-05-29 21:45:27 +0400

    added a comment to show the endpoint for getting all transac

--- server-side/routes/transactionRoutes.js
+++ server-side/routes/transactionRoutes.js
@@ -5,7 +5,7 @@ const authenticateToken = require('../middlewares/authMiddleware');
 const router = express.Router();
 
 router.post('/create', authenticateToken, transactionController.createTransaction); // Endpoint to create a new transaction
-router.get('/', authenticateToken, transactionController.getTransactions);
+router.get('/', authenticateToken, transactionController.getTransactions); //  Endpoint to get all transactions
 router.delete('/delete', authenticateToken, transactionController.deleteTransaction);
 
 module.exports = router;
