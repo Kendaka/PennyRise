8092d59021ac79b5ee6e6684310d25d02c5caa7c
Kendaka <barengkendrick4@gmail.com>
2025-05-30 21:25:29 +0400

    endpoint that update user onnboarding status

--- server-side/routes/userRoutes.js
+++ server-side/routes/userRoutes.js
@@ -9,7 +9,7 @@ router.post('/login', userController.login); // Endpoint to log in an existing u
 router.put('/update-income-currency', authenticateToken, userController.updateIncomeAndCurrency); // Endpoint to update user's income and currency
 router.put('/update-profile', authenticateToken, userController.updateProfile); // Endpoint to update user's profile information
 router.put('/update-profile-picture', authenticateToken, userController.upload.single('profilePicture'), userController.updateProfilePicture);
-router.put('/update-onboarding-status', authenticateToken, userController.updateOnboardingStatus);
+router.put('/update-onboarding-status', authenticateToken, userController.updateOnboardingStatus); // Endpoint to update user's onboarding status
 router.put('/change-password', authenticateToken, userController.changePassword);
 router.get('/profile', authenticateToken, userController.getProfile);
 router.put('/reset-monthly-data', userController.resetMonthlyData);
